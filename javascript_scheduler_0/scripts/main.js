49fe09591e2199fa2e1eb7bff6063156c132e806
Jason <walzak@gmail.com>
2016-11-18 00:14:12 -0500

    Initial Commit

---
+++ javascript_scheduler_0/scripts/main.js
@@ -0,0 +1,12 @@
+﻿//Will create the table dynamically
+
+var row = 4;
+var col = 7;
+function loadTable() {
+    for (var i = 0; i < row; i++) {
+        document.getElementById("schedTable").innerHTML += "<tr id=row" + i + "><td>Week " + (i + 1) + "</td>";
+        for (var j = 0; j < col; j++) {
+            document.getElementById('row'+i).innerHTML += "<td id=" + (i + j) + ">data</td></tr>";
+        }
+    }
+}
