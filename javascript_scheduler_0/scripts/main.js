20b55caa96ab1f34b6876840c627c181dc5b304a
Jason <walzak@gmail.com>
2016-11-19 23:07:51 -0500

    Added an onlick to eachcell of the table For temp storage there is an alert and the data in the cell changes.

--- javascript_scheduler_0/scripts/main.js
+++ javascript_scheduler_0/scripts/main.js
@@ -6,7 +6,14 @@ function loadTable() {
     for (var i = 0; i < row; i++) {
         document.getElementById("schedTable").innerHTML += "<tr id=row" + i + "><td>Week " + (i + 1) + "</td>";
         for (var j = 0; j < col; j++) {
-            document.getElementById('row'+i).innerHTML += "<td id=" + (i + j) + ">data</td></tr>";
+            document.getElementById('row' + i).innerHTML += '<td id="' + (i + j) + '"onclick="insertData(this);">Data</td>';
         }
     }
+}
+
+
+//The function to change the table to store the information that the user adds to the text area.
+function insertData(ref) {
+    alert("it worked");
+    ref.innerHTML = "Data Inserted";
 }
